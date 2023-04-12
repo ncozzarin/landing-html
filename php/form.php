5b5cd3b75f55759667a87550cd7c901ffd183c14
Nicolas Cozzarin <11572984+ncozzarin@users.noreply.github.com>
2023-04-12 11:15:08 +0200

    Update form.php

--- php/form.php
+++ php/form.php
@@ -1,19 +1,17 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-echo "script executed";
-
+ah c
 if(isset($_POST['name']) && isset($_POST['_replyto']) && isset($_POST['message'])) {
-    echo "hola";
+
     $to = "nicolasjcozzarin@gmail.com"; // this is the email address you want to send the form to
     $from = $_POST['_replyto']; // this is the sender's email address
     $name = $_POST['name'];
     $subject = "New message from your website";
     $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
-
     $headers = "From:" . $from;
     if(mail($to,$subject,$message,$headers)){
-        echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+        echo "<script>alert('Mail Sent. Thank you, we will contact you shortly.');</script>";
     } else {
         echo "There was an error sending the email.";
     }
