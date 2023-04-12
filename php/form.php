307541114d1646cd41b025e4f8053b484a083909
Nicolas Cozzarin <11572984+ncozzarin@users.noreply.github.com>
2023-04-12 10:01:27 +0200

    Update form.php

--- php/form.php
+++ php/form.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 echo "script executed";
 var_dump($_POST);
-if(isset($_POST['send'])){
+if(isset($_POST['name'])){
     echo "hola";
     $to = "nicolas.cozzarin@swissds.ch"; // this is the email address you want to send the form to
     $from = $_POST['email']; // this is the sender's email address
