3365c17393da720b63cc72f95528ec0a2b5499ec
Nicolas Cozzarin <11572984+ncozzarin@users.noreply.github.com>
2023-04-12 09:33:28 +0200

    Update form.php

--- php/form.php
+++ php/form.php
@@ -1,13 +1,19 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 if(isset($_POST['submit'])){
-    $to = "nicolasjcozzarin@gmail.com"; // this is the email address you want to send the form to
+    $to = "nicolas.cozzarin@swissds.ch"; // this is the email address you want to send the form to
     $from = $_POST['email']; // this is the sender's email address
     $name = $_POST['name'];
     $subject = "New message from your website";
     $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
-    mail($to,$subject,$message,$headers);
-    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    if(mail($to,$subject,$message,$headers)){
+        echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    } else {
+        echo "There was an error sending the email.";
+    }
 }
 ?>
