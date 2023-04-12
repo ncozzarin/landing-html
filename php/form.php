3d211d5e236a7ddd5c0129ff5da65b06e615d9a2
Nicolas Cozzarin <11572984+ncozzarin@users.noreply.github.com>
2023-04-12 10:24:45 +0200

    update form

--- php/form.php
+++ php/form.php
@@ -2,10 +2,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 echo "script executed";
-var_dump($_POST);
-if(isset($_POST['name'])){
+
+if(isset($_POST['name']) && isset($_POST['_replyto']) && isset($_POST['message'])) {
     echo "hola";
-    $to = "nicolas.cozzarin@swissds.ch"; // this is the email address you want to send the form to
+    $to = "nicolasjcozzarin@gmail.com"; // this is the email address you want to send the form to
     $from = $_POST['_replyto']; // this is the sender's email address
     $name = $_POST['name'];
     $subject = "New message from your website";
