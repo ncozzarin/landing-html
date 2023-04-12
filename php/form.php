da781f212e6cc2363aca556a8aeb09ca1ab75f58
Nicolas Cozzarin <11572984+ncozzarin@users.noreply.github.com>
2023-04-12 09:28:35 +0200

    updating form functionality

--- php/form.php
+++ php/form.php
@@ -1,12 +1,13 @@
 <?php
-if(isset($_POST['submit'])) {
-  $to = "nicolasjcozzarin@gmail.com";
-  $name = $_POST['name'];
-  $email = $_POST['_replyto'];
-  $message = $_POST['message'];
-  $subject = "New Form Submission";
-  $headers = "From: $email";
-  mail($to, $subject, $message, $headers);
-  header("Location: index.html");
+if(isset($_POST['submit'])){
+    $to = "nicolasjcozzarin@gmail.com"; // this is the email address you want to send the form to
+    $from = $_POST['email']; // this is the sender's email address
+    $name = $_POST['name'];
+    $subject = "New message from your website";
+    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
 }
 ?>
