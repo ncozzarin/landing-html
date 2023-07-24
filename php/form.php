e2a32677f073ebc4c7a071c97f5a08f815e34f4e
Nicolas Cozzarin <11572984+ncozzarin@users.noreply.github.com>
2023-07-24 15:44:07 +0200

    hello form

--- php/form.php
+++ php/form.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 
 if(isset($_POST['name']) && isset($_POST['_replyto']) && isset($_POST['message'])) {
 
-    $to = "nicolasjcozzarin@gmail.com"; // this is the email address you want to send the form to
+    $to = "hello@swissds.ch"; // this is the email address you want to send the form to
     $from = $_POST['_replyto']; // this is the sender's email address
     $name = $_POST['name'];
     $subject = "New message from your website";
